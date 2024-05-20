8b6a2202347a6f4ac2fb46c8d5c8ce232e3bbe91
trujo90 <truji90@gmail.com>
2024-05-20 16:17:15 +0100

    connection to deb succesful

--- Telechiste-3000/database/index.js
+++ Telechiste-3000/database/index.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
+
+const connection = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
@@ -8,3 +9,18 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
 });
+
+const checkDB = async () => {
+    try {
+        await connection.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+
+}
+
+module.exports = {
+    connection,
+    checkDB
+}
